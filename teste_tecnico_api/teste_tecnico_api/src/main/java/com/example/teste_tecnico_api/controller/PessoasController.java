5e5d0246586e52c4383a2d29b0418680ef877f3a
Marcos-Barry <rosario.marcosv@gmail.com>
2021-09-23 00:46:58 -0300

    terminando PessoasController

--- teste_tecnico_api/teste_tecnico_api/src/main/java/com/example/teste_tecnico_api/controller/PessoasController.java
+++ teste_tecnico_api/teste_tecnico_api/src/main/java/com/example/teste_tecnico_api/controller/PessoasController.java
@@ -1,40 +1,62 @@
 package com.example.teste_tecnico_api.controller;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.teste_tecnico_api.model.entities.Pessoas;
+import com.example.teste_tecnico_api.service.PessoaService;
 
 
+@RequestMapping("/pessoa")
 public class PessoasController implements ControllerInterfaces<Pessoas> {
 
+	@Autowired
+	private PessoaService service;
+	
 	@Override
 	public ResponseEntity<List<Pessoas>> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return ResponseEntity.ok(service.findAll());
 	}
 
 	@Override
-	public ResponseEntity<?> get(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+	@GetMapping(value="/{id}")
+	public ResponseEntity<?> get(@PathVariable("id")  Long id) {
+			Pessoas _pessoa = service.findById(id);
+			return null;
+			
 	}
 
 	@Override
-	public ResponseEntity<Pessoas> post(Pessoas obj) {
-		// TODO Auto-generated method stub
-		return null;
+	@PostMapping
+	public ResponseEntity<Pessoas> post(@RequestBody Pessoas obj) {
+		service.create(obj);
+		return ResponseEntity.ok(obj);
 	}
 
 	@Override
 	public ResponseEntity<?> put(Pessoas obj) {
-		// TODO Auto-generated method stub
-		return null;
+		if(service.update(obj)) {
+			return ResponseEntity.ok(obj);
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 
 	@Override
+	@DeleteMapping(value="/{id}")
 	public ResponseEntity<?> delete(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		if(service.delete(id)) {
+			return ResponseEntity.ok().build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 
 }

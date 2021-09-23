5f9fc94f8c45ff22f32411c5f97f54e61c70c730
Marcos-Barry <rosario.marcosv@gmail.com>
2021-09-22 23:54:08 -0300

    criando api em java

---
+++ teste_tecnico_api/teste_tecnico_api/src/main/java/com/example/teste_tecnico_api/TesteTecnicoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.teste_tecnico_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTecnicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTecnicoApiApplication.class, args);
+	}
+
+}
